885a05f17f1a8efefcbb9c13fdbed895fb3dc9cc
Dherkyan R. Silva <dherkyan@gmail.com>
2026-02-16 04:44:33 -0300

    26. Router Link e Material Icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {MatButton} from "@angular/material/button";
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {MatToolbar} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, MatButton, MatToolbar],
+    imports: [RouterOutlet, RouterLink, MatToolbar, MatIconModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     standalone: true
