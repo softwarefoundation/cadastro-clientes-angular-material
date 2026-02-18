922c67219dc2800b225e26900358d9ebafca743f
Dherkyan R. Silva <dherkyan@gmail.com>
2026-02-18 02:29:04 -0300

    31. Criando o repositorio de clientes para persistir os dados

--- src/app/shared/services/cliente.service.ts
+++ src/app/shared/services/cliente.service.ts
@@ -6,12 +6,29 @@ import {Cliente} from "../../cadastro/cliente";
 })
 export class ClienteService {
 
+
+    static REPO_CLIENTES = "_CLIENTES";
+
     constructor() {
     }
 
 
     salvar(cliente: Cliente) {
         console.log('Salvando cliente: ', cliente);
+    }
+
+
+    obterStorage(): Cliente[] {
+        const respositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
+
+        if (respositorioClientes) {
+            return JSON.parse(respositorioClientes);
+        }
+
+        const clientes: Cliente[] = [];
+        localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes));
+        return clientes;
+
     }
 
 }
